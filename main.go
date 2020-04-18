74dc106f5cdc96dc42668aeb436493858b470e00
Brendon Dulam <brendondulam06@gmail.com>
2020-04-18 13:09:35 +0900

    success test

--- main.go
+++ main.go
@@ -88,10 +88,8 @@ func main() {
 	})
 
 	router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world")
+		c.String(http.StatusOK, "hello world2")
 	})
-
-	fail
 
 	router.GET("/repeat", repeatHandler(repeat))
 
