875898c2b5c420b911198f47f581d1d6d66e686d
Brendon Dulam <brendondulam06@gmail.com>
2020-04-18 20:17:17 +0900

    ticket service

--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"bytes"
 	"database/sql"
-	"fmt"
+	v1 "github.com/bldulam1/parking-service/api/v1"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
-	_ "github.com/lib/pq"
 )
 
 func repeatHandler(r int) gin.HandlerFunc {
@@ -22,40 +20,6 @@ func repeatHandler(r int) gin.HandlerFunc {
 			buffer.WriteString("Hello from Go!\n")
 		}
 		c.String(http.StatusOK, buffer.String())
-	}
-}
-
-func dbFunc(db *sql.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
-			return
-		}
-
-		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error incrementing tick: %q", err))
-			return
-		}
-
-		rows, err := db.Query("SELECT tick FROM ticks")
-		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
-			return
-		}
-
-		defer rows.Close()
-		for rows.Next() {
-			var tick time.Time
-			if err := rows.Scan(&tick); err != nil {
-				c.String(http.StatusInternalServerError,
-					fmt.Sprintf("Error scanning ticks: %q", err))
-				return
-			}
-			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-		}
 	}
 }
 
@@ -93,7 +57,7 @@ func main() {
 
 	router.GET("/repeat", repeatHandler(repeat))
 
-	router.GET("/db", dbFunc(db))
+	router.GET("/api/v1/ticket", v1.DbFunc(db))
 
 	router.Run(":" + port)
 }
