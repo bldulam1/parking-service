5b5ad038fd585540ef84d9f000a4d643804eb71c
Brendon Dulam <brendondulam06@gmail.com>
2020-04-20 04:38:07 +0900

    sqlStatement

--- api/v1/ticket.service.go
+++ api/v1/ticket.service.go
@@ -6,12 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	"time"
 )
 
 func CreateTicket(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//vehicle, parkingSlot := c.Query("vehicle"), c.Query("parkingSlot")
 		var ticket Ticket
+		var sqlStatement string
 		if err := c.BindJSON(&ticket); err != nil {
 			c.String(http.StatusBadRequest, "Failed to bind request")
 		}
@@ -26,27 +28,33 @@ func CreateTicket(db *sql.DB) gin.HandlerFunc {
 		}
 
 		//Create table if not exists
-		if _, err := db.Exec(`
-			CREATE TABLE IF NOT EXISTS tickets (
-				id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-				time_entry timestamp DEFAULT now(),
-				time_exit timestamp,
-				vehicle varchar NOT NULL,
-				parkingSlot varchar NOT NULL
-			)
-		`); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
+		sqlStatement = `
+            CREATE TABLE IF NOT EXISTS tickets (
+                id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+                time_entry timestamp DEFAULT now(),
+                time_exit timestamp,
+                vehicle varchar NOT NULL,
+                parkingSlot varchar NOT NULL
+            )`
+		if _, err := db.Exec(sqlStatement); err != nil {
+			errString := fmt.Sprintf("Error creating database table: %q", err)
+			c.String(http.StatusInternalServerError, errString)
 			return
 		}
 
 		//Insert new ticket into tickets
-		if _, err := db.Exec(fmt.Sprintf(
-			"INSERT INTO tickets (vehicle, parkingSlot) VALUES ('%s', '%s')", ticket.Vehicle, ticket.ParkingSlot)); err != nil {
-
+		ticket.TimeEntry = time.Now()
+		sqlStatement = `
+			INSERT INTO tickets (vehicle, parkingSlot, time_entry) 
+			VALUES ($1, $2, $3) RETURNING id`
+		if err := db.QueryRow(sqlStatement,
+			ticket.Vehicle,
+			ticket.ParkingSlot,
+			ticket.TimeEntry.Format(time.RFC3339Nano),
+		).Scan(&ticket.Id); err != nil {
 			c.String(http.StatusInternalServerError, fmt.Sprintf("Error inserting ticket: %q", err))
-			return
 		}
+		fmt.Println(ticket.Id, ticket.TimeEntry)
 
 		c.JSON(http.StatusOK, string(ticket.JSON()))
 	}
@@ -54,10 +62,12 @@ func CreateTicket(db *sql.DB) gin.HandlerFunc {
 
 func GetTickets(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		rows, err := db.Query("SELECT id FROM tickets")
+		var sqlStatement string
+		sqlStatement = "SELECT * FROM tickets"
+
+		rows, err := db.Query(sqlStatement)
 		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error reading ticks: %q", err))
 			return
 		}
 
